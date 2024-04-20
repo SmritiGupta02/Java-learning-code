bb14405b235edf0ae937e319c8f4d57ff7a277f0
abhisheksahu <abhishek.sahu@outlook.com>
2024-04-20 08:50:32 +0200

    Commit First

---
+++ src/ex_20042024/lab05.java
@@ -0,0 +1,9 @@
+package ex_20042024;
+
+public class lab05 {
+    public static void main(String[] args) {
+        int age_pramod = 34;
+        int age_reena = 28;
+        boolean result = age_pramod > age_reena;
+        System.out.println(result);
+    }}
