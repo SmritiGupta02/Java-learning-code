e18344f964abee0fe209532cf1edcca194ace70f
abhisheksahu <abhishek.sahu@outlook.com>
2024-04-20 09:03:23 +0200

    Commit First

---
+++ src/ex_20042024/lab06.java
@@ -0,0 +1,10 @@
+package ex_20042024;
+
+public class lab06 {
+    public static void main(String[] args) {
+        int x = 10;
+        float y = 12.4f;
+        float z = 56.78f;
+        System.out.println(x*x);
+
+    }}
