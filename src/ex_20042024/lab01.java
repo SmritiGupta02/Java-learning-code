4f248752b2545918a0c77be0039bcb460b5fcb5c
abhisheksahu <abhishek.sahu@outlook.com>
2024-04-20 08:40:30 +0200

    Commit First

---
+++ src/ex_20042024/lab01.java
@@ -0,0 +1,7 @@
+package ex_20042024;
+
+public class lab01 {
+    public static void main(String[] args) {
+        System.out.println("Anika");
+        System.out.println("Samriddhi");
+    }}
---
+++ src/ex_20042024/lab02.java
@@ -0,0 +1,7 @@
+package ex_20042024;
+
+public class lab02 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+    }}
