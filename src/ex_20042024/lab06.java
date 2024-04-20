dd70311c6f1244ab0480da5b6d5d2f1a157168fa
abhisheksahu <abhishek.sahu@outlook.com>
2024-04-20 09:23:07 +0200

    Commit First

--- src/ex_20042024/lab06.java
+++ src/ex_20042024/lab06.java
@@ -2,9 +2,14 @@ package ex_20042024;
 
 public class lab06 {
     public static void main(String[] args) {
+
+
+       // Question 4
         int x = 10;
         float y = 12.4f;
         float z = 56.78f;
-        System.out.println(x*x);
+        double sum = Math.pow(x,2) + Math.pow(y,2) - Math.abs(z);
+        double result = Math.cbrt(sum);
+        System.out.println(result);
 
     }}
