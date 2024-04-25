7834e03ae6f3793a39dafc7b2f13cbb3f7aa355b
abhisheksahu <abhishek.sahu@outlook.com>
2024-04-25 22:22:57 +0200

    Commit First

---
+++ src/ex_23042024/lab12.java
@@ -0,0 +1,15 @@
+package ex_23042024;
+import java.util.Scanner;
+public class lab12 {
+    // homework positive or negative number
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int num = sc.nextInt();
+        if (num>=0)
+        {
+            System.out.println("Positive number");}
+            else
+        { System.out.println("Negative number");}
+    }
+}
